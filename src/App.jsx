e7805e05e82ebaf2903b0768132f923f401b5f26
Muhammed Aslan <aslanmuhammed2534@gmail.com>
2023-11-30 14:31:28 +0300

    login feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { PageContainer } from "./Components/GridWiev";
 import LoginSignUp from "./Components/LoginSignUp/LoginSignUp";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
-    <PageContainer>
+    <div className="appDiv">
+      <ReactPlayer
+        url="https://minio.asggrup.com/storage/Root/5239-glomil/TEKNOLOJI_WEB.mp4"
+        playing
+        loop
+        muted
+        width="100%"
+        height="100%"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: "-1",
+        }}
+      />
       <LoginSignUp />
-    </PageContainer>
+    </div>
   );
 }
 
