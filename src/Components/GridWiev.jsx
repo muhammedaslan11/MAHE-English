f1cc8f5d39aade9a6824251f7b4f4e628419faa2
ErenBerkeGez <eren.gez123@gmail.com>
2023-12-01 16:33:13 +0300

    userinfo

--- src/Components/GridWiev.jsx
+++ src/Components/GridWiev.jsx
@@ -22,7 +22,7 @@ export const Container = ({ children, overflow }) => {
         boxShadow:
           "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
       }}
-      className={`h-full max-w-screen-xl px-12 py-4 flex ${o}`}
+      className={`h-full flex-col max-w-screen-xl px-12 py-4 flex ${o}`}
     >
       {children}
     </div>
